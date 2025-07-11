2f5dd13ba504a8449fdfc5a3bae07b6adea9539d
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-07-11 21:05:17 +0530

    hello

--- src/components/Header1.jsx
+++ src/components/Header1.jsx
@@ -1,24 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Header1 = () => {
   return (
-    <><div className=" he ">
-    <div className="mainHb STRING1  w-full ">
-        <div className="text  flex">
- <button className="h-full text-[25px] mt-3  font-stretch-ultra-condensed  font-bold ml-96  text-[#FD520F] ">
-            Contact us
-          </button>
-          <button className="h-full STRING1 text-[25px]   font-stretch-ultra-condensed mt-3  ml-56 font-bold  text-[#FD520F] ">
-            Home
-          </button>
-          <button className="h-full ml-64 text-[25px] font-stretch-ultra-condensed  mt-2 font-bold  text-[#FD520F] ">
-            About us
-          </button></div>
-    </div></div>
-    
-    
-    </>
-  )
-}
+    <div className="he">
+      <div className="mainHb STRING1 w-full">
+        <div className="flex justify-center items-center gap-48 mt-4">
+          <div className="text ml-10">About Us</div>
+          <div className="text ml-10">Home</div>
+          <div className="text">Contact Us</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header1
+export default Header1;
