0e5e429396914b2aea0ca2c78251aaa5d4641000
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-07-11 18:04:15 +0530

    hello

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,19 @@ import "./components.css";
 const Navbar = ({ height }) => {
   return (
     <>
-      <div className=" he mb-20 "style={{ fontFamily: "'Afacad', Helvetica" }}>
-    <div className="mainHb  w-full ">
-        <div className="text  flex">
- <button className="h-full text-[25px] mt-3  font-stretch-ultra-condensed  font-bold ml-96  text-[#FD520F] ">
-            Contact us
-          </button>
-          <button className="h-full text-[25px]   font-stretch-ultra-condensed mt-3  ml-52 font-bold  text-[#FD520F] ">
-            Home
-          </button>
-          <button className="h-full ml-60 text-[25px] font-stretch-ultra-condensed  mt-2 font-bold  text-[#FD520F] ">
-            About us
-          </button></div>
-    </div></div>
+      <div style={{ fontFamily: "'Afacad', Helvetica" }}>
+        <div className="mainHb w-full bg-cover bg-center py-4">
+          <div className="flex justify-center  flex-wrap gap-52">
+            <button className="text-2xl  font-bold text-[#FD520F]">
+              Contact Us
+            </button>
+            <button className="text-2xl  font-bold text-[#FD520F]">Home</button>
+            <button className="text-2xl   font-bold text-[#FD520F]">
+              About Us
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
