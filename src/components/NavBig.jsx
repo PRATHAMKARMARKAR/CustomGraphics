e3d5eefe945cd6f7686384b8a4acedbd3344c5b6
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-07-11 20:42:54 +0530

    hello

--- src/components/NavBig.jsx
+++ src/components/NavBig.jsx
@@ -9,7 +9,7 @@ const NavBig = ({ height }) => {
  <button className="h-full text-sm mt-3  font-stretch-ultra-condensed  font-bold ml-12  text-[#b0acab] ">
             Contact us
           </button>
-          <button className="h-full text-lg  font-stretch-ultra-condensed mt-3  ml-14 font-bold  text-[#b0acab] ">
+          <button className="h-full w-5 text-lg  font-stretch-ultra-condensed mt-3  ml-14 font-bold  text-[#b0acab] ">
             Home
           </button>
           <button className="h-full ml-20 text-lg font-stretch-ultra-condensed  mt-2 font-bold  text-[#b0acab] ">
