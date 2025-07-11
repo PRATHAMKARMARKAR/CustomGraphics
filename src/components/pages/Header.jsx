3e3d4af7ffb035a80362adfac1e88aa4d58ef512
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-07-11 21:37:11 +0530

    hello

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
      <div className="main h-20 overflow-hidden w-full  text-black flex items-center  justify-between ">
    <div className="left centerSection1 "></div>
    <div className="right flex gap-20 ml-8 mr-10 text-[#FD520F]  font-medium md:text-xl text-sm " style={{ fontFamily: "'Afacad', sans-serif" }}>
-    <h1>Contact us</h1>
-    <h1>Home</h1>
-    <h1>About us</h1>
+     <div className="text ml-10  w-30">About Us</div>
+           <div className="text ml-10 w-30">Home</div>
+           <div className="text  w-30">Contact Us</div>
    </div>
      </div>
     </>
