148bd8a1b95e62dc2c02d2e779afd4a850d3cbbe
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-07-11 20:58:18 +0530

    hello

--- src/components/NavBig.jsx
+++ src/components/NavBig.jsx
@@ -5,11 +5,11 @@ const NavBig = ({ height }) => {
 <>
 <div className=" he h-[25px] "style={{ fontFamily: "'Afacad', Helvetica" }}>
     <div className="mainHP  w-full ">
-        <div className="text-xs mt-4 flex">
+        <div className="text-xs  mt-4 flex">
  <button className="h-full    font-stretch-ultra-condensed  font-bold ml-12  text-[#b0acab] ">
             Contact us
           </button>
-          <button className="h-full   font-stretch-ultra-condensed   ml-14 font-bold  text-[#b0acab] ">
+          <button className="  font-stretch-ultra-condensed   ml-14 font-bold  text-[#b0acab] ">
             Home
           </button>
           <button className="h-full ml-20  font-stretch-ultra-condensed   font-bold  text-[#b0acab] ">
