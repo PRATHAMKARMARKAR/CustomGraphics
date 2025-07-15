0893a9ea7bf9c34bb1b5d05c2a3fee8ea6e25bfe
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-07-15 17:10:22 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import ElementsPage from './components/pages/ElementsPage'
 import Header1 from './components/Header1'
 import OurServices from './components/pages/OurServices'
 const App = () => {
+    const heightNavbar = 20;
+  const marginT = 30;
   return (
     <>
     <Body/>
@@ -12,6 +14,7 @@ const App = () => {
     {/* <Header1/> */}
     {/* <ElementsPage/> */}
     {/* <OurServices/> */}
+     <Footer height={heightNavbar} margin={marginT} />
     </>
   )
 }
