e5d414e072b61ffc9da0f83979fb506175a0891c
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-07-17 11:10:42 +0530

    hello

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
 const Header = () => {
+    const navigate = useNavigate();
   return (
     <>
      <div className="main h-20 overflow-hidden w-full  text-black flex items-center  justify-between ">
    <div className="left centerSection1 "></div>
    <div className="right flex gap-20 ml-8 mr-10 text-[#FD520F]  font-medium md:text-xl text-xs " style={{ fontFamily: "'Afacad', sans-serif" }}>
     <h1>Contact us</h1>
-    <h1>Home</h1>
+    <h1   onClick={() => navigate('/')}
+      className='cursor-pointer'>Home</h1>
     <h1>About us</h1>
    </div>
      </div>
