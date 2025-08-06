18cbfede28f8af0d653707768fde2aae85098487
ayeeesaket <saketvermawid@gmail.com>
2025-08-06 23:02:35 +0530

    pulling

--- src/components/pages/ElementsPage.jsx
+++ src/components/pages/ElementsPage.jsx
@@ -14,23 +14,23 @@ const ElementsPage = () => {
         <Headerone />
         <div className="flex text-[#FD520F] overflow-hidden">
           <div className="logo -mt-14 centerSection2"> </div>
-          <div className="STRINGONE "></div>
+          <div className="STRINGONE 2xl:-mt-8 ml-10 "></div>
           <div className="   STRING1   -ml-12  md:mb-14  md:h-full   mt-5 md:mt-2  "></div>
-          <div className="bookbutton h-7 w-32 border-2 mt-16  overflow-hidden   md:hover:text-gray-400 md:hover:border-[#FD520F]   -ml-60 md:h-10 md:w-52  md:px-3 text-center md:-ml-[1250px] 2xl:-mr-10  rounded-4xl bg-white md:mt-[330px] "
+          <div className="bookbutton h-7 w-32 border-2 mt-16  overflow-hidden   md:hover:text-gray-400 md:hover:border-[#FD520F]  py-1 -ml-60 md:h-10 md:w-52  md:px-3 text-center md:-ml-[1250px] 2xl:-mr-10  rounded-4xl bg-white md:mt-[330px] 2xl:mt-[290px] "
            onClick={()  => navigate("/Ourservice")
             }>
             Our Services
           </div>
           <div className="  mt-[60px] -ml-24  "></div>
-          <div className="bookbutton h-7 w-32 text-center rounded-4xl border-2 md:hover:text-gray-400 md:hover:border-[#FD520F]  md:py-1 -ml-10 md:h-10 md:w-52 md:ml-[180px]  md:mt-2 bg-white  mt-32 ">
+          <div className="bookbutton h-7 w-32 text-center rounded-4xl border-2 md:hover:text-gray-400 md:hover:border-[#FD520F]  md:py-1 -ml-10 md:h-10 md:w-52 md:ml-[180px]  md:mt-2 bg-white  2xl:mt-0  ">
             Our Projects
           </div>
           <div className="  mt-[125px] -ml-20 2xl:-ml-26  "></div>
-          <div className="bookbutton h-7 w-32 text-center rounded-4xl border-2 md:hover:text-gray-400 md:hover:border-[#FD520F]  md:py-1 -ml-1  md:h-10 md:w-52  md:ml-[290px]  md:mt-[530px] bg-white mt-50 2xl:mt-130 ">
+          <div className="bookbutton h-7 w-32 text-center rounded-4xl border-2 md:hover:text-gray-400 md:hover:border-[#FD520F]  md:py-1 -ml-1  md:h-10 md:w-52  md:ml-[290px]  md:mt-[530px] bg-white mt-50 2xl:mt-120 2xl:ml-80 ">
             Testimonials
           </div>
           <div className=" mt-52 -ml-20  2xl:-ml-5"> </div>
-          <div className="bookbutton h-7 w-32 text-center rounded-4xl border-2 md:hover:text-gray-400 md:hover:border-[#FD520F] md:py-1 ml-4 md:h-10  md:w-52 md:ml-[130px] bg-white md:mt-[120px]  mt-[320px]  ">
+          <div className="bookbutton h-7 w-32 text-center rounded-4xl border-2 md:hover:text-gray-400 md:hover:border-[#FD520F] md:py-1 ml-4 md:h-10  md:w-52 md:ml-[130px] bg-white md:mt-[120px]  mt-[320px] 2xl:mt-24 2xl:mr-20 ">
             Our Products
           </div>
           <div className=" mt-[300px] -ml-10  "></div>
